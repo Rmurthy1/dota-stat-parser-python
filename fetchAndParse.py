c70a96a6181a1c76360da8bc6f9f8105595eec79
Rmurthy1 <rahul.a.murthy@gmail.com>
2015-06-09 22:51:13 -0700

    sql working properly

--- fetchAndParse.py
+++ fetchAndParse.py
@@ -13,18 +13,23 @@ import sqlite3
 # currently only has an id for each player
 class Player:
     def __init__(self, info):
-        self.ID = info
+        self.match_id = info["account_id"]
+        self.player_slot = info["player_slot"]
+        self.hero_id = info["hero_id"]
     def __repr__(self):
-        return str(self.ID)
+        return str(self.match_id)
 
 
 class Match:
     def __init__(self, info):
-        self.ID = info
+        self.match_id = info["match_id"]
+        self.players = info["player_list"]
+        self.start_time = info["start_time"]
     def __repr__(self):
-        return int(self.ID)
+        return int(self.match_id)
     def __str__(self):
-        return str(self.ID)
+        return (str(self.match_id) + str(self.start_time))
+
 
 def main():
     matchList = []
@@ -36,10 +41,18 @@ def main():
 
     print (data["result"]["results_remaining"])
     for match in data["result"]["matches"]:
+        match_info = {}
+        match_info["match_id"] = match["match_id"]
+        match_info["start_time"] = match["start_time"]
+        player_list = []
+        for player in match["players"]:
+            player_list.append(Player(player))
+
+        match_info["player_list"] = player_list
         tempString = match["match_id"]
         #print (tempString)
         # make an unnamed match and throw it in the list of matches
-        matchList.append(Match(tempString))
+        matchList.append(Match(match_info))
 
     # show me all the matches
     for match in matchList:
@@ -57,6 +70,7 @@ def main():
         data = cur.fetchone()
 
         print("SQLite version: %s" % data)
+        cur.execute("DROP TABLE IF EXISTS Matches")
 
         stmt = "PRAGMA table_info(Matches)"
         cur.execute(stmt)
@@ -70,13 +84,14 @@ def main():
         #for all the matches in the list, insert them into the table
         for match in matchList:
 			# note the trailing comma for a single element tuple
-            cur.execute('INSERT INTO Matches VALUES(?)', (match.ID,))
+            cur.execute('INSERT INTO Matches VALUES(?)', (match.match_id,))
 
 		# now try to get em back out!
-
         cur.execute("SELECT * FROM Matches")
 
         rows = cur.fetchall()
+
+        con.commit()
 
         for row in rows:
             print (row[0])
